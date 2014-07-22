c3e2725cec501bda1a978d4c1d60c82621e92bbf
kecinzer <kecinzer@gmail.com>
2014-07-22 15:06:19 +0200

    (3/3) Contact details on incoming call
    Change-Id: I932714de440df471371f08f2b60be7d2bcbab2cf
    Signed-off-by: kecinzer <kecinzer@gmail.com>
    
    Conflicts:
    	res/values/strings_custom.xml
    	res/xml/call_feature_setting.xml
    	src/com/android/phone/CallFeaturesSetting.java

--- src/com/android/phone/CallFeaturesSetting.java
+++ src/com/android/phone/CallFeaturesSetting.java
@@ -223,6 +223,8 @@ public class CallFeaturesSetting extends PreferenceActivity
             "button_choose_people_lookup_provider";
     private static final String BUTTON_CHOOSE_REVERSE_LOOKUP_PROVIDER =
             "button_choose_reverse_lookup_provider";
+    private static final String BUTTON_DETAILED_INCALL_INFO_KEY =
+            "button_detailed_incall_info";
 
     private Intent mContactListIntent;
 
@@ -326,6 +328,7 @@ public class CallFeaturesSetting extends PreferenceActivity
     private ListPreference mChooseForwardLookupProvider;
     private ListPreference mChoosePeopleLookupProvider;
     private ListPreference mChooseReverseLookupProvider;
+    private CheckBoxPreference mDetailedIncallInfo;
 
     private class VoiceMailProvider {
         public VoiceMailProvider(String name, Intent intent) {
@@ -631,6 +634,10 @@ public class CallFeaturesSetting extends PreferenceActivity
             Settings.System.putInt(mPhone.getContext().getContentResolver(),
                     Settings.System.CALL_UI_IN_BACKGROUND,
                     (Boolean) objValue ? 1 : 0);
+        } else if (preference == mDetailedIncallInfo){
+            Settings.System.putInt(getContentResolver(), Settings.System.DETAILED_INCALL_INFO,
+                    mDetailedIncallInfo.isChecked() ? 1 : 0);
+            return true;
         } else if (preference == mIncallGlowpadTransparency) {
             Settings.System.putInt(mPhone.getContext().getContentResolver(),
                     Settings.System.INCALL_GLOWPAD_TRANSPARENCY,
@@ -1816,6 +1823,10 @@ public class CallFeaturesSetting extends PreferenceActivity
         mChooseReverseLookupProvider.setOnPreferenceChangeListener(this);
 
         restoreLookupProviders();
+
+        mDetailedIncallInfo = (CheckBoxPreference) findPreference(BUTTON_DETAILED_INCALL_INFO_KEY);
+        mDetailedIncallInfo.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.DETAILED_INCALL_INFO, 0) != 0 ? true : false);
 
         // create intent to bring up contact list
         mContactListIntent = new Intent(Intent.ACTION_GET_CONTENT);
